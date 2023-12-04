dc2a74d52480f08a8fb5c34443253642ba035bdd
loptopitop <rodrigo.elustondo@gmail.com>
2023-12-03 22:25:25 -0500

    API gateway complete

---
+++ spring-social/api-gateway/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "ca.gbc"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-gateway
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.0.8")
+    // https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-netflix-eureka-client
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.3")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- spring-social/settings.gradle.kts
+++ spring-social/settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "spring-social"
 
-include("friendship-service", "social-service","discovery-service")
+include("friendship-service", "social-service","discovery-service","api-gateway")
